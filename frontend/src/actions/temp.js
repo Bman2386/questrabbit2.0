351cfa7ce8b806d91dc5992bded0287c1ebbf3ef
Brendon <brendon.biagi@gmail.com>
2023-02-13 17:59:49 -0500

    remove old code

--- frontend/src/actions/temp.js
+++
@@ -1,9 +0,0 @@
-
-export const RECEIVE_DATA = 'RECEIVE_DATA';
-
-const recieveData = data => ({
-    type: RECEIVE_DATA,
-    data
-});
-
-export const saveData = data => dispatch=> dispatch(recieveData(data));
