1cc48b9b9081c80045020183af907359f9ecab9c
Brendon <brendon.biagi@gmail.com>
2023-03-15 11:49:09 -0400

    fix warnings in footer component

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -5,9 +5,20 @@ import GitHub from '../../images/GitHub.png';
 function Footer() {
         return (
             <div className="footer">
-                <a target="_blank" href="https://www.linkedin.com/in/brendonbiagi/" className='a'><img src={linkedin} className="linked"/></a>
-                <a target="_blank" href="https://github.com/Bman2386" className='a'><img src={GitHub} className="linked2"/></a>
-                <a target="_blank" href="https://bman2386.github.io/Brendon.Biagi/" className="creator">By Brendon Biagi</a>
+                <a target="_blank" 
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/brendonbiagi/" 
+                    className='a'><img src={linkedin} alt='linkedin logo'className="linked"/>
+                </a>
+                <a target="_blank" 
+                    rel="noreferrer"
+                    href="https://github.com/Bman2386" 
+                    className='a'><img src={GitHub} alt='github logo'className="linked2"/>
+                </a>
+                <a target="_blank" 
+                    rel="noreferrer"
+                    href="https://bman2386.github.io/Brendon.Biagi/" 
+                    className="creator">By Brendon Biagi</a>
             </div>
         );
     };
