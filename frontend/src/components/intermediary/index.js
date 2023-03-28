2981b129d1552dca04a1c99dc9f78028dc17b824
Brendon <brendon.biagi@gmail.com>
2023-03-28 12:31:04 -0400

    add alt tags to intermediary componenet

--- frontend/src/components/intermediary/index.js
+++ frontend/src/components/intermediary/index.js
@@ -6,10 +6,10 @@ import formphoto from '../../images/formphoto.jpg';
 function Intermediary(){
     return (
         <div className="session-form">
-            <img src={formphoto} className="form-photo"/>
+            <img src={formphoto} alt='background' className="form-photo"/>
             <form className="inter-form" id="form1">
                 <Link to='/'>
-                    <img src={QuestRabbit} className="logo"/>
+                    <img src={QuestRabbit} alt='logo' className="logo"/>
                 </Link>
                 <Link className="btn-1" to="/signup">Sign Up</Link>
                 <Link className="btn-2" to="/login">Log In</Link>
