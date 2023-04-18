6ed054d53815378e6a40d0c361d95aa217fbccff
Brendon <brendon.biagi@gmail.com>
2023-04-18 12:45:01 -0400

    remove extraneous else statement, add a comment to questStart (1st step of creating a quest, child of PartOne)

--- frontend/src/components/createQuest/partOneUtils/questStart.js
+++ frontend/src/components/createQuest/partOneUtils/questStart.js
@@ -2,6 +2,7 @@ import React from "react";
 import pencil from '../../../images/pencil.png';
 
 export function QuestStart({mini, setQuestName, questName, setMini}){
+    // if user is just starting form, mini is 0, and render this form, otherwise render line 26 
     if (mini === 0) {
         return (
             <div className="quest-name">
@@ -21,13 +22,12 @@ export function QuestStart({mini, setQuestName, questName, setMini}){
 
             </div>
         );
-    } else {
-        return (
+    }; 
+    return (
             <div className='quest-name'>
                 <img className='pencil'alt='pencil' src={pencil} id='pencil' onClick={() => setMini(0)} />
                 <div className='label'>Name of your Quest:</div>
                 <div className='orders2'>{`${questName}`}</div>
             </div>
         );
-    };
 };
