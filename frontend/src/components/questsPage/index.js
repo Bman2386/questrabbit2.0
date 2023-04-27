88c5287ad1cc665949319dcd3343f1842734f2c1
Brendon <brendon.biagi@gmail.com>
2023-04-27 12:32:49 -0400

    move questShow function to seperate concerns

--- frontend/src/components/questsPage/index.js
+++ frontend/src/components/questsPage/index.js
@@ -2,7 +2,8 @@ import React,{useState, useEffect, useRef} from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { fetchQuests } from '../../store/quest';
-import { categoryShow, adventurerShow, dateShow } from '../../utils/show';
+// import { categoryShow, adventurerShow, dateShow } from '../../utils/show';
+import { QuestShow } from './questShow';
 import EditQuest from './edit';
 import CancelQuest from './cancel';
 import './questPage.css';
@@ -40,74 +41,16 @@ function QuestsPage (){
         const matchedQuest = temp[0];
         setQuest(matchedQuest);
         setCancel(true)
-    } 
-    function questShow() {
-            if (quests.length > 0){
-            return (
-                <div className='show-quests' id='background'>
-                    <div className='h1' id='background'>Your Quests</div>
-                    {quests.map(quest =>
-                        <div key={quest.id} className='quest-name'>
-                            <div className='links2'>
-                                <div className='p'> Quest Name:</div>
-                                <div className='orders4'>
-                                    {quest.questName}
-                                </div>
-                            </div>
-                            <div className='links2'>
-                                <div className='p'>Details:</div>
-                                <div className='orders4'>
-                                    "{quest.details}"
-                                </div>
-                            </div>
-                            <div className='links2'>
-                                <div className='p'>Start Time:</div>
-                                <div>
-                                    <div className='orders4'>{dateShow(quest.startTime)}</div>
-                                </div>
-                            </div>
-                            <div className='links2'>
-                                <div className='p'>Category: </div>
-                                <div className='orders4'>
-                                    {categoryShow(quest.categoryId)}
-                                </div>
-                            </div>
-                            <div className='links2'>
-                                <div className='p'>Adventurer:</div>
-                                <div className='orders4'>
-                                    {adventurerShow(quest.adventurerId)}
-                                </div>
-                            </div>
-                            <div className='links2' id='center'>
-                                <button
-                                    className="btn-1"
-                                    value={quest.id}
-                                    onClick={(e) => moveToEdit(e.target.value)}
-                                >Edit Quest</button>
-                                <button
-                                    to={`/delete/${quest.id}`}
-                                    className="cancel-btn"
-                                    value={quest.id}
-                                    onClick={e => moveToCancel(e.target.value)}
-                                >Cancel Quest</button>
-                            </div>
-                        </div>
-                    )}
-                </div>
-                );
-            }; 
-               return( //if no active quests, encourage user to create one
-                   <div className="quest-name">
-                       <h1 className='h1' id='center'>Have something else on your to-do list?</h1>
-                       <p className='p'>Book your next Quest or manage future to-dos  with Quest Rabbit</p>
-                       <Link to="/quest"className='button-submit' id='center'>Check It Off Your List</Link>
-                   </div>
-               );
-        };
+    };
+    
         return(
-            <div>
-                <div className="form2" id='background'>{questShow()}</div>
-            </div>
+                <div className="form2" id='background'>
+                    <QuestShow
+                    quests={quests}
+                    moveToEdit={moveToEdit}
+                    moveToCancel={moveToCancel}
+                    />
+                </div>
         );
 };
 
---
+++ frontend/src/components/questsPage/questShow.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import { categoryShow, adventurerShow, dateShow } from '../../utils/show';
+
+export function QuestShow({quests, moveToEdit, moveToCancel}){
+    if (quests.length > 0) {
+        return (
+            <div className='show-quests' id='background'>
+                <div className='h1' id='background'>Your Quests</div>
+                {quests.map(quest =>
+                    <div key={quest.id} className='quest-name'>
+                        <div className='links2'>
+                            <div className='p'> Quest Name:</div>
+                            <div className='orders4'>
+                                {quest.questName}
+                            </div>
+                        </div>
+                        <div className='links2'>
+                            <div className='p'>Details:</div>
+                            <div className='orders4'>
+                                "{quest.details}"
+                            </div>
+                        </div>
+                        <div className='links2'>
+                            <div className='p'>Start Time:</div>
+                            <div>
+                                <div className='orders4'>{dateShow(quest.startTime)}</div>
+                            </div>
+                        </div>
+                        <div className='links2'>
+                            <div className='p'>Category: </div>
+                            <div className='orders4'>
+                                {categoryShow(quest.categoryId)}
+                            </div>
+                        </div>
+                        <div className='links2'>
+                            <div className='p'>Adventurer:</div>
+                            <div className='orders4'>
+                                {adventurerShow(quest.adventurerId)}
+                            </div>
+                        </div>
+                        <div className='links2' id='center'>
+                            <button
+                                className="btn-1"
+                                value={quest.id}
+                                onClick={(e) => moveToEdit(e.target.value)}
+                            >Edit Quest</button>
+                            <button
+                                to={`/delete/${quest.id}`}
+                                className="cancel-btn"
+                                value={quest.id}
+                                onClick={e => moveToCancel(e.target.value)}
+                            >Cancel Quest</button>
+                        </div>
+                    </div>
+                )}
+            </div>
+        )};
+    return ( //if no active quests, encourage user to create one
+        <div className="quest-name">
+            <h1 className='h1' id='center'>Have something else on your to-do list?</h1>
+            <p className='p'>Book your next Quest or manage future to-dos  with Quest Rabbit</p>
+            <Link to="/quest" className='button-submit' id='center'>Check It Off Your List</Link>
+        </div>
+    );
+}
