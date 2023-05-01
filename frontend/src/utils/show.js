415922d57d3a09a7ec1ce786d8ee7f9021b46526
Brendon <brendon.biagi@gmail.com>
2023-05-01 17:15:35 -0400

    fix typo in dateShow

--- frontend/src/utils/show.js
+++ frontend/src/utils/show.js
@@ -16,7 +16,7 @@ export const dateShow = (date) => {
         "Tuesday",
         "Wednesday",
         "Thursday",
-        "Firday",
+        "Friday",
         "Saturday"
     ];
     const months = [
