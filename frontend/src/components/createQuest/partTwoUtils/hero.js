7e2451178d3eabb07faaadd4f0a6418974bf9f67
Brendon <brendon.biagi@gmail.com>
2023-07-06 12:51:30 -0400

    refactor Hero component to 1 if statement

--- frontend/src/components/createQuest/partTwoUtils/hero.js
+++ frontend/src/components/createQuest/partTwoUtils/hero.js
@@ -6,8 +6,15 @@ import goblin_slayer from '../../../images/goblin_slayer.jpg';
 // returns an image of adventurer avatar
 // will need to refactor at scale
 export function Hero({name}){
-    if (name === 'Hercules') return <img src={hercules} alt='hero' className='adv-img' />;
-    if (name === 'Goblin Slayer') return <img src={goblin_slayer} alt='hero' className='adv-img' />;
-    if (name === 'Isaac Newton') return <img src={isaac_newton} alt='hero' className='adv-img' />;
-    return <div className='error'>Error with name</div>;
+    const adventurers = {
+        'Hercules': hercules,
+        'Goblin Slayer': goblin_slayer,
+        'Isaac Newton': isaac_newton
+    };
+    if (!adventurers[name]) return <div className='error'>Error with name</div>;
+    return <img src={adventurers[name]}  alt='hero' className='adv-img' />;
+    // if (name === 'Hercules') return <img src={hercules} alt='hero' className='adv-img' />;
+    // if (name === 'Goblin Slayer') return <img src={goblin_slayer} alt='hero' className='adv-img' />;
+    // if (name === 'Isaac Newton') return <img src={isaac_newton} alt='hero' className='adv-img' />;
+    // return <div className='error'>Error with name</div>;
 };
