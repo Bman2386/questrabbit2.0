059c065723ac741fd1c44b64be2d210749eb52d1
Brendon <brendon.biagi@gmail.com>
2023-07-06 13:00:13 -0400

    fix className missmatch in splash page

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -69,8 +69,9 @@ const lastButton = () => {
       </div>
       <div className='store'> 
         <img src={apple} alt='apple logo' id='apple'></img>
-        <div className='fake-logo-apple-logo'>
-          <div className='small'>Available in the</div> Magic Store
+        <div className='fake-logo'>
+          <div className='small'>Available in the</div>
+          Magic Store
         </div>
       </div>
       <div className='store'>
