c55cfd492d5a51e8cd749df5c156f14450e54078
Brendon <brendon.biagi@gmail.com>
2023-07-06 12:38:31 -0400

    clarify sortBy componenet

--- frontend/src/components/createQuest/partTwoUtils/sortBy.js
+++ frontend/src/components/createQuest/partTwoUtils/sortBy.js
@@ -1,43 +1,38 @@
 import React from "react";
 
-export function SortBy({adventurers, examine, setAdv, setIsChecked}){
+export function SortBy({adventurers, examine, setAdventurersArray, setIsChecked}){
 
     const cloneOfAdventurers = [...adventurers];
 
-    const sortHelper = (type, arr=cloneOfAdventurers)=> {
-        const first = arr[0]; //using recursion to sort adventurers by desired stat
-        if (arr.length < 2) return arr;
-        const func = (x,y) => {
-            if (x>y) return -1;
+    const sortHelper = (type, clonedArrayOfAdventurers=cloneOfAdventurers)=> {
+        const firstAdventurer = clonedArrayOfAdventurers[0]; //using recursion to sort adventurers by desired stat
+        if (clonedArrayOfAdventurers.length < 2) return clonedArrayOfAdventurers;
+        const sortFunction = (first,second) => {
+            if (first > second) return -1;
             return 1;
         };
-        let left = arr.slice(1).filter(el => func(el[type], first[type]) === -1);
-        let right = arr.slice(1).filter(el => func(el[type], first[type]) !== -1);
+        let left = clonedArrayOfAdventurers.slice(1).filter(adventurer => sortFunction(adventurer[type], firstAdventurer[type]) === -1);
+        let right = clonedArrayOfAdventurers.slice(1).filter(adventurer => sortFunction(adventurer[type], firstAdventurer[type]) !== -1);
         left = sortHelper(type, left);
         right = sortHelper(type, right);
 
-        return left.concat([first]).concat(right);
+        return left.concat([firstAdventurer]).concat(right);
     };
 
-    const sortAdv = (sortType)=> {
-        const sortTypes = {
-            reviews: 'totalRatings',
-            high: 'avgRating',
-            recommended: 'id' 
-        };
-        const sorted = sortHelper(sortTypes[`${sortType}`]);
-        setAdv(sorted);
+    const sortAdventuerers = (sortType)=> {
+        const sorted = sortHelper(sortType);
+        setAdventurersArray(sorted);
         setIsChecked(false);
     };
     if (!examine) {
             return (
                 <div className='sorting'>
                     <div className='sort'>Sort By:</div>
-                    <select onChange={(e) => sortAdv(e.target.value)}>
+                    <select onChange={(e) => sortAdventuerers(e.target.value)}>
                         <option value="">-</option>
-                        <option value='recommended'>Best Value</option>
-                        <option value="reviews">Most Reviewed</option>
-                        <option value="high">Highest Rating</option>
+                        <option value='id'>Best Value</option>
+                        <option value="totalRatings">Most Reviewed</option>
+                        <option value="avgRating">Highest Rating</option>
                     </select>
                 </div>
             );
