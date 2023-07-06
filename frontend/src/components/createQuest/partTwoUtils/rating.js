0bcd679a8301a017e4773066d1a7e5fb03f312b6
Brendon <brendon.biagi@gmail.com>
2023-07-06 12:43:28 -0400

    clarify rating component

--- frontend/src/components/createQuest/partTwoUtils/rating.js
+++ frontend/src/components/createQuest/partTwoUtils/rating.js
@@ -2,12 +2,12 @@ import React from "react";
 import star from '../../../images/star.png';
 
 export function Rating({rating = 1}){
-    let num = Number(rating);
-    const stars = [];
-    for (let i = num; i > 0; i--) stars.push(i);
+    let starRating = Number(rating);
+    const starsCountArray = [];
+    for (let i = starRating; i > 0; i--) starsCountArray.push(i);
     return (
         <div className='rating'>
-            {stars.map((idx) => <img src={star} alt='star' className='star' key={idx} />)}
+            {starsCountArray.map((index) => <img src={star} alt='star' className='star' key={index} />)}
         </div>
     );
 };
